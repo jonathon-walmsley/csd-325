dd60db89b6266d51e177aedd63deab5c0f4ebf6d
Jonathon Walmsley <Jonathon.Walmsley@gmail.com>
2026-08-01 11:42:21 -0700

    Pushing Module-11

---
+++ module-11/demo/myapp/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render, HttpResponse
+from .models import TodoItem
+
+# Create your views here.
+def home(request):
+    return render(request, 'home.html')
+
+def todos(request):
+    return render(request, 'todos.html', {'todos': TodoItem.objects.all()})
